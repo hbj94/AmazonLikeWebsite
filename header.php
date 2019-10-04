fb2cf1d5b0242fde13b2a740cff73acb1a44d2c8
Tino <tino_teros@hotmail.com>
2019-10-04 17:32:54 -0400

    Initial Commit: A base format.
    A base format that we can use to get started. 
    We will need more php files and javaScript files when we start making the functions.

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+	
+
+	<head>
+		<title>354 The Stars</title>
+
+		<link rel="stylesheet" type="text/css" href="Style.css">
+		
+		<meta charset="utf-8">
+
+		<script type="text/javascript" src="someJavaScriptFile.js"> 
+		</script>
+
+	</head>
+
+
+
+	<body>
+
+			<table >
+
+					<td> <h1> MarketPlace -  354 The Stars</h1>
+					</td>
+
+			</table>	
